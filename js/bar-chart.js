ea3d3c841207d30188c9a1b6faf69ab5411db780
William Lim <manvinedi@gmail.com>
2017-01-01 10:46:36 +0800

    first commit

---
+++ js/bar-chart.js
@@ -0,0 +1,8 @@
+var data = [4, 8, 20, 16, 23, 42];
+
+d3.select(".chart")
+  .selectAll("div")
+    .data(data)
+  .enter().append("div")
+    .style("width", function(d) { return d * 10 + "px"; })
+    .text(function(d) { return d; });
