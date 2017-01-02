d9a98f966016b100a105ed2a2e61bc032f2bf734
William Lim <manvinedi@gmail.com>
2017-01-02 14:16:55 +0800

    d3 tsv works now

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
 
 // var routes = require('./routes/index');
 // var users = require('./routes/users');
@@ -16,13 +17,14 @@ app.use(express.static('public'));
 //   res.send('Hello World!');
 // })
 
+
 // Default route.
-app.get('/', function(req, res){
-  res.sendfile('index.html');
+app.get('/', function(request, response){
+  response.sendfile('index.html');
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function(request, response, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -33,8 +35,8 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
+  app.use(function(err, request, response, next) {
+    response.status(err.status || 500);
     // res.render('error', {
     //   message: err.message,
     //   error: err
@@ -44,8 +46,8 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
+app.use(function(err, request, response, next) {
+  response.status(err.status || 500);
   // res.render('error', {
   //   message: err.message,
   //   error: {}
