37ca4eee4bfe60db342a58502bc9d334878f4791
William Lim <manvinedi@gmail.com>
2018-05-19 15:58:32 +0800

    updates to bar chart 3

--- src/tutorial/components/bar-chart-3/bar-chart-3.js
+++ src/tutorial/components/bar-chart-3/bar-chart-3.js
@@ -7,7 +7,10 @@ var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
     y = d3.scaleLinear().rangeRound([height, 0]);
 
 var g = svg.append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr(
+      "transform",
+      "translate(" + margin.left + "," + margin.top + ")"
+    )
 
 d3.tsv("data.tsv",
   function(d) {
